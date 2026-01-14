b0cd1dfb2c98e19aa38cb3d2e2cd77788aae146c
netenebrae <netenebrae@proton.me>
2026-01-14 15:42:45 -0500

    vault backup: 2026-01-14 15:42:45

--- quartz/plugins/emitters/favicon.ts
+++ quartz/plugins/emitters/favicon.ts
@@ -20,4 +20,3 @@ export const Favicon: QuartzEmitterPlugin = () => ({
   },
   async *partialEmit() {},
 })
-
--- quartz/plugins/emitters/ogImage.tsx
+++ quartz/plugins/emitters/ogImage.tsx
@@ -14,7 +14,7 @@ import fs from "node:fs/promises"
 import { styleText } from "util"
 
 const defaultOptions: SocialImageOptions = {
-  colorScheme: "darkMode",
+  colorScheme: "lightMode",
   width: 1200,
   height: 630,
   imageStructure: defaultImage,
